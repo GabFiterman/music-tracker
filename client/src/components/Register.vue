f4e632736bd80e13f76d4e2408955871235d5bb6
Fiterman <gfiterman96@gmail.com>
2023-05-04 23:21:58 -0300

    connecting to sqlite using sequelize and freshening up the register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,45 +1,86 @@
 <template>
-  <div>
-    <h1>Register</h1>
-
-    <input
-      type="text"
-      name="email"
-      v-model="email"
-      placeholder="email"
-    />
-    <br />
-    <input
-      type="text"
-      name="password"
-      v-model="password"
-      placeholder="password"
-    />
-    <br />
-    <button @click="register">Register</button>
-  </div>
+  <v-container>
+    <v-layout style="justify-content: center">
+      <v-flex xs6>
+        <v-card>
+          <v-card-title>Register</v-card-title>
+          <div class="inputGroup">
+            <input
+              type="text"
+              name="email"
+              v-model="email"
+              placeholder="email"
+            />
+            <br />
+            <input
+              type="password"
+              name="password"
+              v-model="password"
+              placeholder="password"
+            />
+          </div>
+          <!-- <p v-if="error" class="error">{{ error }}</p> -->
+          <div v-html="error" class="error" />
+          <v-btn class="inputButton" @click="register">Register</v-btn>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import AuthenticationService from '../../../server/services/AuthenticationService'
+import AuthenticationService from "../../../server/services/AuthenticationService";
 export default {
-  name: 'Register',
-  data () {
+  name: "Register",
+  data() {
     return {
       email: undefined,
-      password: undefined
+      password: undefined,
+      error: null
     };
   },
   methods: {
     async register() {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
+        this.error = null;
+      } catch (err) {
+        this.error = err.response.data.error;
+      }
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.error {
+  color: #c90000;
+  font-style: italic;
+  font-size: 0.85rem;
+}
+
+.v-card__title {
+  justify-content: center;
+  font-size: 2rem;
+  font-weight: bold;
+  color: #008e00;
+}
+
+.inputGroup input {
+  outline: 1px solid #cccccc;
+  padding: 0.5rem 1rem;
+  border-radius: 2rem;
+  margin: .5rem 0;
+}
+
+.inputGroup input:focus {
+  outline: 1px solid #595959;
+}
+
+.inputButton {
+  margin: 2rem 0;
+}
+</style>
---
+++ server/routes.js
@@ -0,0 +1,10 @@
+const AuthenticationController = require("./src/controllers/AuthenticationController");
+const AuthenticationControllerPolicy = require("./src/policies/AuthenticationControllerPolicy");
+
+module.exports = (app) => {
+  app.post(
+    "/register",
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register
+  );
+};
