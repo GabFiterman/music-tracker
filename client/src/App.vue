afe0e919de10cf16dfcf2d316b3057b9a46fbb83
Fiterman <gfiterman96@gmail.com>
2023-05-05 19:56:09 -0300

    improving header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app id="app">
-    <Header />
-    <router-view/>
+    <v-container fluid px-0 py-0>
+      <Header v-if="showHeader" />
+        <main :class="showHeader ? 'Header__margin__ajust' : ''">
+          <router-view/>
+        </main>
+    </v-container>
   </v-app>
 </template>
 
@@ -12,16 +16,38 @@ export default {
   name: 'App',
   components: {
     Header
+  },
+  computed: {
+    showHeader () {
+      const actualRoute = this.$route.name.toLowerCase()
+      return actualRoute !== 'register' && actualRoute !== 'login'
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #362c50;
+
+  .Header__margin__ajust {
+    margin-top: 10vh;
+  }
+
+  input {
+    outline: 1px solid #4d0f56;
+    padding: 0.5rem 1rem;
+    border-radius: 2rem;
+    width: 25vw;
+  }
+
+  input:focus {
+    outline: 1px solid #E8D4ED;
+  }
 }
+
 </style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,8 +1,32 @@
 <template>
-  <v-toolbar fixed elevation="1">
+  <v-toolbar elevation="1" class="Header purple darken-3 white--text">
     <v-toolbar-title>
-      Music tracker
+      <router-link to="/" class="Header__logo">
+        Music tracker
+      </router-link>
     </v-toolbar-title>
+
+    <v-toolbar-items style="margin-left: 2vw;">
+      <v-btn elevation="0" class="Header__button" dark>
+        Browse
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn elevation="0" class="Header__button" dark>
+        <router-link to="/login">
+          Login
+        </router-link>
+      </v-btn>
+
+      <v-btn elevation="0" class="Header__button" dark>
+        <router-link to="/register">
+          Sign Up
+        </router-link>
+      </v-btn>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
 
@@ -12,4 +36,23 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.Header {
+  position: fixed;
+  width: 100vw;
+
+  .Header__logo:hover {
+    cursor: pointer;
+    color: #f7a1db;
+  }
+
+  .Header__button {
+    background-color: #ffffff00 !important;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+}
+</style>
