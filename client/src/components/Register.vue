6c5c38732513d686260556bc0c13a627a7e08d36
Fiterman <gfiterman96@gmail.com>
2023-05-05 15:56:36 -0300

    css ajusts

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,31 +1,33 @@
 <template>
-  <v-container>
-    <v-layout style="justify-content: center">
-      <v-flex xs6>
-        <v-card>
-          <v-card-title>Register</v-card-title>
-          <div class="inputGroup">
-            <input
-              type="text"
-              name="email"
-              v-model="email"
-              placeholder="email"
-            />
-            <br />
-            <input
-              type="password"
-              name="password"
-              v-model="password"
-              placeholder="password"
-            />
-          </div>
-          <!-- <p v-if="error" class="error">{{ error }}</p> -->
-          <div v-html="error" class="error" />
-          <v-btn class="inputButton" @click="register">Register</v-btn>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div class="Register">
+    <v-container>
+      <v-layout style="justify-content: center">
+        <v-flex xs6>
+          <v-card class="register__card">
+            <v-card-title class="register__card--title">Register</v-card-title>
+            <div class="register__card--inputGroup">
+              <input
+                type="text"
+                name="email"
+                v-model="email"
+                placeholder="email"
+              />
+              <br />
+              <input
+                type="password"
+                name="password"
+                v-model="password"
+                placeholder="password"
+              />
+            </div>
+            <!-- TODO: verify another way to show text with html tags. Simple {{ don't works }}  -->
+            <div v-html="error" class="register__error" />
+            <v-btn class="register__card--button" @click="register">Register</v-btn>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -56,31 +58,53 @@ export default {
 </script>
 
 <style scoped>
-.error {
+.Register {
+  height: 100vh;
+  width: 100vw;
+  background-color: #E8D4ED;
+}
+
+.register__card {
+  min-height: 65vh;
+  max-width: 35vw;
+  margin: 0 auto;
+  margin-top: 15vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.register__card--title {
+  justify-content: center;
+  font-size: 2rem;
+  font-weight: bold;
+  color: #4d0f56;
+}
+
+.register__card--inputGroup input{
+  outline: 1px solid #4d0f56;
+  padding: 0.5rem 1rem;
+  border-radius: 2rem;
+  margin: 2vh 0;
+  width: 25vw;
+}
+
+.register__card--inputGroup input:focus {
+  outline: 1px solid #4d0f56;
+}
+
+.register__error {
   color: #c90000;
   font-style: italic;
   font-size: 0.85rem;
 }
 
-.v-card__title {
-  justify-content: center;
-  font-size: 2rem;
-  font-weight: bold;
-  color: #008e00;
-}
-
-.inputGroup input {
-  outline: 1px solid #cccccc;
-  padding: 0.5rem 1rem;
-  border-radius: 2rem;
-  margin: .5rem 0;
-}
-
-.inputGroup input:focus {
-  outline: 1px solid #595959;
-}
-
-.inputButton {
+.register__card--button {
   margin: 2rem 0;
+  width: 15vw;
+  border-radius: 2rem;
+  background-color: #4d0f56 !important;
+  color: white;
 }
 </style>
